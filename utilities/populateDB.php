29a8ba5d7054bf3e08b68896beb000ead55c07b0
Alicja <puranut@hotmail.com>
2021-07-12 15:13:39 +0100

    made it work

--- utilities/populateDB.php
+++ utilities/populateDB.php
@@ -5,30 +5,34 @@ curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
 $response = curl_exec($request);
 $data = json_decode($response);
 
-$db = new PDO('mysql:host=db; dbname=koalaCars', 'root', 'password');
+$db = new PDO('mysql:host=127.0.0.1;dbname=koalaCars', 'root', 'password');
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $query = $db->prepare("DROP TABLE cars");
 $query->execute();
-$query = $db->prepare("CREATE TABLE cars");
+$query = $db->prepare(
+        "CREATE TABLE `cars` (
+      `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+      `make` varchar(255) NOT NULL DEFAULT '',
+      `model` varchar(255) NOT NULL DEFAULT '',
+      `year` int(4) NULL,
+      `color` varchar(10) NULL DEFAULT '',
+      `location` varchar(255) NOT NULL DEFAULT '',
+      `image` varchar(10) NOT NULL DEFAULT '',
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=latin1;"
+);
 $query->execute();
 
 foreach ($data as $car) {
 
-    $id = $car['id'];
-    $make = $car['make'];
-    $model = $car['model'];
-    $year = $car['year'];
-    $color = $car['color'];
-    $location = $car['location'];
-    $image = $car['image'];
-
     $query = $db->prepare("INSERT INTO `cars` (`id`, `make`, `model`, `year`, `color`, `location`, `image`) 
-            VALUES (:id, :make, :model, :year, :color, :location. :image)");
-    $query->execute([':id' => $id,
-                    ':make' => $make,
-                    ':model' => $model,
-                    ':year' => $year,
-                    ':color' => $color,
-                    ':location' => $location,
-                    ':image' => $image
+            VALUES (:id, :make, :model, :year, :color, :location, :image)");
+    $query->execute([':id' => $car->id,
+                    ':make' => $car->make,
+                    ':model' => $car->model,
+                    ':year' => $car->year,
+                    ':color' => $car->color,
+                    ':location' => $car->location,
+                    ':image' => $car->image
     ]);
 }
