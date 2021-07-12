6c0b688f254541f160c9c725b24b4381e9111abf
Alicja <puranut@hotmail.com>
2021-07-12 13:41:31 +0100

    creating a folder structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'vendor/autoload.php';
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+</head>
+<body>
+
+</body>
+</html>
