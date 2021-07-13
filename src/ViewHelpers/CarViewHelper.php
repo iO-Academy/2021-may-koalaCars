455481ff542d2333cb469fd91e4a18f99a877888
Lucy Ferrabee <lucy.ferrabee@hotmail.com>
2021-07-13 10:54:39 +0100

    created a viewhelper for displaying the details for just one car

--- src/ViewHelpers/CarViewHelper.php
+++ src/ViewHelpers/CarViewHelper.php
@@ -19,4 +19,21 @@ class CarViewHelper
         }
         return $output;
     }
+
+    public static function displayCarDetails(object $car): string
+    {
+        $details = '';
+        if (is_object($car)) {
+            $details .= '<div class="details"><h1>Make: </h1>' . $car->getMake() .
+                '<h1>Model: </h1>' . $car->getModel() .
+                '<h1>Year: </h1>' . $car->getYear() .
+                '<h1>Color: </h1>' . $car->getColor() .
+                '<h1>Location: </h1>' . $car->getLocation() . '</div>';
+            $details .= '<div class="car"><img src="https://dev.io-academy.uk/resources/cars/' . $car->getImage(). '"</div>';
+
+        } else {
+            return 'invalid information';
+        }
+        return $details;
+    }
 }
