c623c88cf8aee497197dc8382cc1ee469da58e59
Lucy Ferrabee <lucy.ferrabee@hotmail.com>
2021-07-13 09:57:48 +0100

    added the image path for images

--- src/ViewHelpers/CarViewHelper.php
+++ src/ViewHelpers/CarViewHelper.php
@@ -9,7 +9,7 @@ class CarViewHelper
         $output = '';
         foreach ($cars as $car) {
             if (is_object($car)) {
-                $output .= '<div class="car_image"><img src="' . $car->getImage() . '"</div>';
+                $output .= '<div class="car_image"><img src="https://dev.io-academy.uk/resources/cars/' . $car->getImage() . '"</div>';
                 $output .= '<div><h1>Make: </h1>' . $car->getMake() . '</div>';
                 $output .= '<div><h1>Model: </h1>' . $car->getModel() . '</div>';
                 $output .= '<div><h1>See more</h1></div>';
