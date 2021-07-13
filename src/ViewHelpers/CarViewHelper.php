9deeaa2eb0eb1a64944135ea97393145c6835bd2
Estera <estera.maftei94@gmail.com>
2021-07-13 17:43:47 +0100

    conflicts resolved

--- src/ViewHelpers/CarViewHelper.php
+++ src/ViewHelpers/CarViewHelper.php
@@ -12,7 +12,7 @@ class CarViewHelper
                 $output .= '<div class="carCards-container"><img src="https://dev.io-academy.uk/resources/cars/' . $car->getImage() . '">';
                 $output .= '<div class="brand-model-container"><h1 class="make">' . $car->getMake() . '</h1>';
                 $output .= '<h1 class="model">' . $car->getModel() . '</h1></div>';
-                $output .= '<div class="link"><a href="#">More Details</a></div></div>';
+                $output .= '<div class="link"><a href="details.php?id=' . $car->getId() . '">More Details</a></div></div>';
             } else {
                 return 'invalid information';
             }
