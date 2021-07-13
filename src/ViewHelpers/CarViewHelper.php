c579f2a17ac55df3beaf0ea1bb872e5043ce3196
Lucy Ferrabee <lucy.ferrabee@hotmail.com>
2021-07-13 09:54:35 +0100

    connected the viewhelper with the CarEntity to display cars on the page

--- src/ViewHelpers/CarViewHelper.php
+++ src/ViewHelpers/CarViewHelper.php
@@ -9,9 +9,9 @@ class CarViewHelper
         $output = '';
         foreach ($cars as $car) {
             if (is_object($car)) {
-                $output .= '<div class="car_image"><img src="' . $car->image . '"</div>';
-                $output .= '<div><h1>Make: </h1>' . $car->make . '</div>';
-                $output .= '<div><h1>Model: </h1>' . $car->model . '</div>';
+                $output .= '<div class="car_image"><img src="' . $car->getImage() . '"</div>';
+                $output .= '<div><h1>Make: </h1>' . $car->getMake() . '</div>';
+                $output .= '<div><h1>Model: </h1>' . $car->getModel() . '</div>';
                 $output .= '<div><h1>See more</h1></div>';
             } else {
                 return 'invalid information';
