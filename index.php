72a9dde70404c0be87776c9c4fe5616247907eac
Alicja <puranut@hotmail.com>
2021-07-13 11:58:06 +0100

    deleted the print_r

--- index.php
+++ index.php
@@ -4,10 +4,6 @@ require_once 'vendor/autoload.php';
 $dbConnection = \KoalaCars\DbConnector::getDb();
 $cars = \KoalaCars\Hydrators\CarHydrator::getCars($dbConnection);
 
-
-echo '<pre>';
-print_r($car);
-echo '</pre>';
 ?>
 
 <html lang="en">
