f6ebc58df187b9b3f607bcc6a20121d72fbc2caf
SniffleSneeze <70772425+SniffleSneeze@users.noreply.github.com>
2021-07-14 15:30:22 +0100

    minor update on css file and class name modified

--- src/ViewHelpers/CarViewHelper.php
+++ src/ViewHelpers/CarViewHelper.php
@@ -11,8 +11,8 @@ class CarViewHelper
     {
         $output = '';
         $output .= '<div class="carCards-container"><img src="https://dev.io-academy.uk/resources/cars/' . $car->getImage() . '">' .
-            '<div class="brand-model-container"><h1 class="make">' . $car->getMake() . '</h1>' .
-            '<h1 class="model">' . $car->getModel() . '</h1></div>' .
+            '<div class="brand-model-container"><h1 class="makeCard">' . $car->getMake() . '</h1>' .
+            '<h1 class="modelCard">' . $car->getModel() . '</h1></div>' .
             '<div class="link"><a href="details.php?id='.$car->getId().'">More Details</a></div></div>';
 
         return $output;
@@ -35,7 +35,7 @@ class CarViewHelper
     public static function displayCarDetails(VehicleAbstract $car): string
     {
         $details = '';
-        $details .= '<div class="carCards-container"><div class="brand-model-container">' .
+        $details .= '<div class="details-carCards-container"><div class="brand-model-container">' .
             '<h1 class="make"><span>Make:</span>' . $car->getMake() . '</h1>'.
             '<h1 class="model"><span>Model:</span>'.$car->getModel() .'</h1>'.
             '<h1 class="year"><span>Year:</span>'.$car->getYear() .'</h1>'.
