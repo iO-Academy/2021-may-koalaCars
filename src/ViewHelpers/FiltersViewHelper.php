3b4da19208cef77c509f84c4339521dc7b8ded3f
Estera <estera.maftei94@gmail.com>
2021-07-14 13:06:18 +0100

    filterss view helper structure changed

--- src/ViewHelpers/FiltersViewHelper.php
+++ src/ViewHelpers/FiltersViewHelper.php
@@ -7,16 +7,17 @@ class FiltersViewHelper
     public static function displayMakes(array $makes, string $selectedMake): string
     {
         $output = '<div class="make-container">';
-
         foreach ($makes as $make) {
-            if (is_object($make)) {
-                $class = '';
-                if ($selectedMake == $make->getMake()) {
-                    $class = ' active';
+            foreach ($make as $value) {
+                if ($value) {
+                    $class = '';
+                    if ($selectedMake == $value) {
+                        $class = ' active';
+                    }
+                    $output .= '<button class="make-btn ' . $class . '"><a href="index.php?make=' . $value . '">' . $value . '</a></button>';
+                } else {
+                    return 'invalid information';
                 }
-                $output .= '<button class="make-btn' . $class . '"><a href="index.php?make=' . $make->getMake() . '">' . $make->getMake() . '</a></button>';
-            } else {
-                return 'invalid information';
             }
         }
         $output .= '</div> ';
