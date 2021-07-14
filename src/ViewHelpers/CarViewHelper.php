b64ff503b9487bb3e68c4dc14e35b94400647556
SniffleSneeze <70772425+SniffleSneeze@users.noreply.github.com>
2021-07-14 10:04:00 +0100

    small css update + private method added inside carViewHelper.php

--- src/ViewHelpers/CarViewHelper.php
+++ src/ViewHelpers/CarViewHelper.php
@@ -1,21 +1,25 @@
 <?php
-
 namespace KoalaCars\ViewHelpers;
+use KoalaCars\Entities\CarEntity;
 
 class CarViewHelper
 {
+    private static function displayCar(CarEntity $car): string
+    {
+        $output = '';
+        $output .= '<div class="carCards-container"><img src="https://dev.io-academy.uk/resources/cars/' . $car->getImage() . '">';
+        $output .= '<div class="brand-model-container"><h1 class="make">' . $car->getMake() . '</h1>';
+        $output .= '<h1 class="model">' . $car->getModel() . '</h1></div>';
+        $output .= '<div class="link"><a href="#">More Details</a></div></div>';
+
+        return $output;
+    }
+
     public static function displayAllCars(array $cars): string
     {
         $output = '';
         foreach ($cars as $car) {
-            if (is_object($car)) {
-                $output .= '<div class="carCards-container"><img src="https://dev.io-academy.uk/resources/cars/' . $car->getImage() . '">';
-                $output .= '<div class="brand-model-container"><h1 class="make">' . $car->getMake() . '</h1>';
-                $output .= '<h1 class="model">' . $car->getModel() . '</h1></div>';
-                $output .= '<div class="link"><a href="#">More Details</a></div></div>';
-            } else {
-                return 'invalid information';
-            }
+            $output .= self::displayCar($car);
         }
         return $output;
     }
